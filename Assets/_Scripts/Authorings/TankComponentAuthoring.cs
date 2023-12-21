625d354806ee19bb349946472dd8ab211512d3a5
alpererdin <alpererdin636@hotmail.com>
2023-12-21 22:06:49 +0300

    ++

--- Assets/_Scripts/Authorings/TankComponentAuthoring.cs
+++ Assets/_Scripts/Authorings/TankComponentAuthoring.cs
@@ -1,12 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
 
 public class TankComponentAuthoring : MonoBehaviour
 {
     public GameObject Explosion;
-    
 }
 
 public class TankComponentBaker : Baker<TankComponentAuthoring>
@@ -14,15 +11,15 @@ public class TankComponentBaker : Baker<TankComponentAuthoring>
     public override void Bake(TankComponentAuthoring authoring)
     {
         var entity = GetEntity(TransformUsageFlags.Dynamic);
+
         AddComponentObject(entity, new TankComponent
         {
             Explosion = authoring.Explosion
-            
         });
+
         AddComponent(entity, new TankEntityTag
         {
             
         });
-        
     }
 }
